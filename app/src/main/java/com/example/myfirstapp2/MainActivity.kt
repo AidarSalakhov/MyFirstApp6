85590a9b2be8aae619f3c048a5da000e2eb72d3e
AidarSalakhov <adr-sal@yandex.ru>
2021-03-31 18:46:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myfirstapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
